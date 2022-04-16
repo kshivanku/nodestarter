c3fd2ab8d9631e166444e486217be3a0b0e64fa5
kshivanku <kshivanku@gmail.com>
2022-04-16 11:39:42 -0700

    using fs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const express = require('express');
+const fs = require('fs');
 const app = express();
 app.listen(8080, ()=> console.log('listening at 8080'))
 app.use(express.static('public'))
@@ -7,6 +8,8 @@ app.use(express.json({limit: '1mb'}))
 
 app.post('/api', (req, res) => {
     console.log(req.body)
+    const data = {lat: req.body.lat, lon: req.body.lon}
+    fs.writeFile('latlon.json', JSON.stringify(data, null, 2), (err) => {err ? console.log(err) : console.log('successfully written file')})
     res.json({
         status: 'success',
         lat: req.body.lat, 
