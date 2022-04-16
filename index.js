d434b97396e824e6ef155a5a057233f73cac029b
kshivanku <kshivanku@gmail.com>
2022-04-16 10:13:44 -0700

    basic handshake

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 app.listen(8080, ()=> console.log('listening at 8080'))
 app.use(express.static('public'))
+app.use(express.json({limit: '1mb'}))
+
+app.post('/api', (req, res) => {
+    console.log(req.body)
+    res.json({
+        status: 'success',
+        lat: req.body.lat, 
+        lon: req.body.lon
+    })
+})
